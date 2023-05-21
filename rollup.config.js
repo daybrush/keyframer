895c73d144632ccf374af58de2cd998b35c17fce
Daybrush <younkue.choi@navercorp.com>
2023-05-22 02:34:10 +0900

    feat: support ssr and  update modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,21 @@
 
-import builder from "@daybrush/builder";
+const builder = require("@daybrush/builder");
 
 const external = {
     "scenejs": "Scene",
 };
-export default builder([
+module.exports = builder([
     {
         name: "Keyframer",
         input: "src/index.ts",
         output: "./dist/keyframer.js",
         exports: "named",
         resolve: true,
-        external,
     },
     {
         name: "Keyframer",
         input: "src/index.ts",
         output: "./dist/keyframer.min.js",
-        exports: "named",
-        resolve: true,
-        uglify: true,
-        external,
-    },
-    {
-        name: "Keyframer",
-        input: "src/index.ts",
-        output: "./dist/keyframer.pkgd.js",
-        exports: "named",
-        resolve: true,
-    },
-    {
-        name: "Keyframer",
-        input: "src/index.ts",
-        output: "./dist/keyframer.pkgd.min.js",
         exports: "named",
         resolve: true,
         uglify: true,
--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
-export const KEYFRAMES_RULE = CSSRule.KEYFRAMES_RULE || (CSSRule as any).WEBKIT_KEYFRAMES_RULE;
-export const KEYFRAME_RULE = CSSRule.KEYFRAME_RULE || (CSSRule as any).WEBKIT_KEYFRAME_RULE;
+
+const hasCSSRule = typeof CSSRule !== "undefined";
+export const KEYFRAMES_RULE = hasCSSRule ? CSSRule.KEYFRAMES_RULE || (CSSRule as any).WEBKIT_KEYFRAMES_RULE : 7;
+export const KEYFRAME_RULE = hasCSSRule ? CSSRule.KEYFRAME_RULE || (CSSRule as any).WEBKIT_KEYFRAME_RULE : 8;
